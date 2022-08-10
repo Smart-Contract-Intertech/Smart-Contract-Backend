3e61d07e426606995ec5ecec66137e1927c708ca
Yüksel Erin <55945878+YukselErin@users.noreply.github.com>
2022-08-10 23:04:30 +0300

    new "it" block

--- tests/mytest.ts
+++ tests/mytest.ts
@@ -2,23 +2,33 @@ import { expect } from "chai";
 import {loadFixture, deployContract} from 'ethereum-waffle';
 import { ethers } from "hardhat";
 import hre from "hardhat";
+async function deployFixture(){                                                                 //Use this to avoid deploying the same contract over and over again for each test
+    const ConditionalInvesment = await hre.ethers.getContractFactory("ConditionalInvesment");   //This is usual contract deployment
+    const cond = await ConditionalInvesment.deploy();
+    return {cond};
+}
 
 describe ("ConditionalInvesment", function () {
-    async function deployFixture(){                                                                 //Use this to avoid deploying the same contract over and over again for each test
-        const ConditionalInvesment = await hre.ethers.getContractFactory("ConditionalInvesment");   //This is usual contract deployment
-        const cond = await ConditionalInvesment.deploy();
-        return {cond};
-    }
+
     it("Should make invesment", async function () {                 
         const { cond } = await loadFixture(deployFixture);              //loadFixture is used to restore the network to a fresh state before each state (loads the saved state)
         const [owner, otherAccount] = await ethers.getSigners();        //This is used to get other addresses in the network, other accounts can be multiple ex. [owner, otherAccount1, otherAccount2]
         
         //Call contract functions
-        cond.register("0xf39Fd6e51aad88F6F4ce6aB8827279cffFb92266");   //!! Overloaded functions does not work for some reason ?
-        cond.registerReceiver(0, "0x70997970C51812dc3A010C7d01b50e0d17dc79C8")
+        cond.register(owner.address);   //!! Overloaded functions does not work for some reason ?
         cond.makeInvesment(otherAccount.address, 1000);
         await expect( cond.connect(otherAccount).withdrawInvesment(0)).to.be.revertedWith("You can not withdraw the funds yet!");    //.to.be.revertedWith means we should get the error message in the brackets
         //The placement of "await" keyword can change, refer to hardhat documentation   
     })
+    it("Should show users invesments", async function (){
+
+        const { cond } = await loadFixture(deployFixture);              //loadFixture is used to restore the network to a fresh state before each state (loads the saved state)
+        const [owner, otherAccount] = await ethers.getSigners();        //This is used to get other addresses in the network, other accounts can be multiple ex. [owner, otherAccount1, otherAccount2]
+        //cond.register(owner.address);                                 //load fixture does not work as expected atm 
+        cond.makeInvesment(otherAccount.address,1000);
+        await new Promise(res => setTimeout(() => res(null), 5000));    //This is needed for ethers to poll the emitted blocks, ?? polling time could be changed also
+        await expect( cond.myInvesments()).to.emit(cond, "InvesmentInfo");//.withArgs(owner.address);
+    })
+
 
 })
